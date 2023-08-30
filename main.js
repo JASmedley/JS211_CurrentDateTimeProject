581920a035a66beac3e6f41e4b9fe71626dbfe36
JASmedley <jasmine.a.smedley@gmail.com>
2023-08-29 23:44:27 -0500

    adding everything

--- main.js
+++ main.js
@@ -1,67 +1,70 @@
-// **THIS IS INCREDIBLY IMPORTANT THAT YOU DO BOTH SECTIONS!!! You will be doing only front-end work in 421 and you need to brush up on your HTML elements**
-
-
 // ***************************
 //          PART ONE
 // ***************************
-// Write a JavaScript program to display the current day and time, start with:
-console.log(new Date)
-
+// EXAMPLE: Write a JavaScript program to display the current day and time, start with:
 const displayDate = () => {
   const currentDate = new Date()
+  document.getElementById("display-element").innerHTML = currentDate
+};
 
-  document.getElementById("display-element").innerHTML = currentDate;
-}
+console.log(new Date)
  
 
-// Write a JavaScript program to convert a number to a string.
+// 1. Write a JavaScript program to convert a number to a string.
+let function1 = function(num){
+  let convertToString = num.toString()
+  document.getElementById("function1").innerHTML = convertToString;
+  console.log(num.toString())
+};
 
+// 2. Write a JavaScript program to convert a string to the number.
+let function2 = function(string){
+  let convertToInt = parseInt(string)
+  document.getElementById("function2").innerHTML = convertToInt;
+  console.log(parseInt(string))
+};
 
-
-// Write a JavaScript program to convert a string to the number.
-
-
-
-// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-  // * Boolean
-  // * Null
-  // * Undefined
-  // * Number
-  // * NaN
-  // * String
+// 3. Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+  let function3 = function(whatIsIt){
+    let getType = (typeof whatIsIt)
+    document.getElementById("function3").innerHTML = getType;
+    console.log(typeof whatIsIt)
+  };
   
-
   
-// Write a JavaScript program that adds 2 numbers together.
+// 4. Write a JavaScript program that adds 2 numbers together.
+let function4 = function(num1, num2) {
+  let addNumbers = (num1 + num2)
+  document.getElementById("function4").innerHTML = addNumbers
+  console.log(num1 + num2)
+};
 
+// 5. Write a JavaScript program that runs only when 2 things are true.
+let function5 = function (thing1, thing2) {
+  let twoTrue = (thing1 && thing2)
+  document.getElementById("function5").innerHTML = twoTrue
 
+  if(thing1 && thing2){
+    console.log(true)
+  }
+}
 
-// Write a JavaScript program that runs only when 2 things are true.
+// 6. Write a JavaScript program that runs when 1 of 2 things are true.
+let function6 = function (thing1, thing2) {
+  let oneTrue = (thing1 || thing2)
+  document.getElementById("function6").innerHTML = oneTrue
 
+  if(thing1 || thing2){
+    console.log(true) }
+}
 
+// 7. Write a JavaScript program that runs when both things are not true.  
 
-// Write a JavaScript program that runs when 1 of 2 things are true.
+let function7 = function (thing1, thing2) {
+  let twoFalse = (thing1 === false && thing2 === false)
+  document.getElementById("function7").innerHTML = twoFalse
 
-
-
-// Write a JavaScript program that runs when both things are not true.  
-
-// ***************************
-//         PART TWO
-// ***************************
-
-// 1. download Live-Server by Ritwick Dey, 
-// 2. reload VS Code, 
-// 3. click the "Go Live" button
-// 4. Go local host 5500 or http://127.0.0.1:5500/index.html to see your web page
-// 5. Or go use the `npm start` command and navigate to localhost:8080 (ctrl + C to close)
-// 6. go to `index.html` 
-// 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
-
-
-
-
-// Additional Resources
-// Video1: https://player.vimeo.com/video/377147232
-// Video2: https://www.youtube.com/embed/bkvH28PXLWc
-// Video3: https://www.youtube.com/embed/TrGI9Yki-24
+  if(thing1 === false && thing2 === false){
+    console.log("The statements are both false: "+true)
+  }
+}
