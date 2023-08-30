3f47dc2c052f7a2211783468692dcf5089109d8f
JASmedley <jasmine.a.smedley@gmail.com>
2023-08-29 23:57:22 -0500

    making everything const instead of function

--- main.js
+++ main.js
@@ -11,37 +11,37 @@ console.log(new Date)
  
 
 // 1. Write a JavaScript program to convert a number to a string.
-let function1 = function(num){
-  let convertToString = num.toString()
+const function1 = (num) =>{
+  const convertToString = num.toString()
   document.getElementById("function1").innerHTML = convertToString;
   console.log(num.toString())
 };
 
 // 2. Write a JavaScript program to convert a string to the number.
-let function2 = function(string){
-  let convertToInt = parseInt(string)
+const function2 = (string) =>{
+  const convertToInt = parseInt(string)
   document.getElementById("function2").innerHTML = convertToInt;
   console.log(parseInt(string))
 };
 
 // 3. Write a JavaScript program that takes in different datatypes and prints out whether they are a:
-  let function3 = function(whatIsIt){
-    let getType = (typeof whatIsIt)
+  const function3 = (whatIsIt) =>{
+    const getType = (typeof whatIsIt)
     document.getElementById("function3").innerHTML = getType;
     console.log(typeof whatIsIt)
   };
   
   
 // 4. Write a JavaScript program that adds 2 numbers together.
-let function4 = function(num1, num2) {
-  let addNumbers = (num1 + num2)
+const function4 = (num1, num2)=> {
+  const addNumbers = (num1 + num2)
   document.getElementById("function4").innerHTML = addNumbers
   console.log(num1 + num2)
 };
 
 // 5. Write a JavaScript program that runs only when 2 things are true.
-let function5 = function (thing1, thing2) {
-  let twoTrue = (thing1 && thing2)
+const function5 =  (thing1, thing2)=> {
+  const twoTrue = (thing1 && thing2)
   document.getElementById("function5").innerHTML = twoTrue
 
   if(thing1 && thing2){
@@ -50,8 +50,8 @@ let function5 = function (thing1, thing2) {
 }
 
 // 6. Write a JavaScript program that runs when 1 of 2 things are true.
-let function6 = function (thing1, thing2) {
-  let oneTrue = (thing1 || thing2)
+const function6 = (thing1, thing2) =>{
+  const oneTrue = (thing1 || thing2)
   document.getElementById("function6").innerHTML = oneTrue
 
   if(thing1 || thing2){
@@ -60,8 +60,8 @@ let function6 = function (thing1, thing2) {
 
 // 7. Write a JavaScript program that runs when both things are not true.  
 
-let function7 = function (thing1, thing2) {
-  let twoFalse = (thing1 === false && thing2 === false)
+const function7 = (thing1, thing2) => {
+  const twoFalse = (thing1 === false && thing2 === false)
   document.getElementById("function7").innerHTML = twoFalse
 
   if(thing1 === false && thing2 === false){
